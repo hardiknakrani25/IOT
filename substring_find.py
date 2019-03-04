1764953f9959e7a76bdd05875b2ce2a3d4d50a7a
Hardik Nakarani <hdnakarani@gmail.com>
2019-03-04 12:09:03 +0530

    changes

--- substring_find.py
+++
@@ -1,7 +0,0 @@
-string = raw_input('Enter String : ')
-substring = raw_input('Search String : ')
- 
-if substring in string:
-	print ("Your substring was found!")
-else:
-	print("NOT find the substring!")
